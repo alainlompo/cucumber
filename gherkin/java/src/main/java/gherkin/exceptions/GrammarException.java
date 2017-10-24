ecf725e7bd6ecc5a30af789741f6b4ff13717289
alainlompo <alain.lompo@gmail.com>
2017-10-24 05:39:10 +0100

    Fix for squid:s1199

---
+++ gherkin/java/src/main/java/gherkin/exceptions/GrammarException.java
@@ -0,0 +1,11 @@
+package gherkin.exceptions;
+
+public class GrammarException extends RuntimeException {
+
+	private static final long serialVersionUID = 5671706261131978994L;
+
+	public GrammarException(String message) {
+		super(message);
+	}
+}
+
